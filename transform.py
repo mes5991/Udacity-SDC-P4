70c3672b850e75b53e896f1520a8ab8078baa07c
mes5991 <mes5991@gmail.com>
2017-02-20 11:55:06 -0500

    tested second transform function

--- transform.py
+++ transform.py
@@ -11,15 +11,20 @@ def undistort(img, mtx, dist):
 def test_transform(folder_path, mtx, dist):
     imgs = os.listdir(folder_path)
     for i in imgs:
+        #Load image
         img_path = folder_path + i
         img = mpimg.imread(img_path)
-        undist = undistort(img, mtx, dist)
-        warped, dst, src = transform(undist)
 
+        #Undistort image based on camera calibration
+        undist = undistort(img, mtx, dist)
+
+        #Transform perspective to birds-eye-view
+        warped, dst, src = transform2(undist)
+
+        #Draw transformation lines and plot
         undist = draw_lines(undist, src, (255,0,0))
         undist = draw_lines(undist, dst, (0,255,0))
         warped = draw_lines(warped, dst, (255,0,0))
-
         plt.subplot(1,3,1)
         plt.imshow(img)
         plt.title("Original Image")
@@ -31,34 +36,47 @@ def test_transform(folder_path, mtx, dist):
         plt.title("Transformed image with transform lines")
         plt.show()
 
-def transform(img):
-    offset = 0
+def transform1(img):
+    offset = 0 #for quick modification
     img_size = (img.shape[1], img.shape[0])
+
+    #points in the real space
     src = np.float32([[(img_size[0] / 2) - 65, img_size[1] / 2 + 100],
                       [((img_size[0] / 6) - 50), img_size[1]],
                       [(img_size[0] * 5 / 6) + 60, img_size[1]],
                       [(img_size[0] / 2 + 65), img_size[1] / 2 + 100]])
+
+    #points on the image plane
     dst = np.float32([[(img_size[0] / 4), 0],
                       [(img_size[0] / 4), img_size[1]],
                       [(img_size[0] * 3 / 4), img_size[1]],
                       [(img_size[0] * 3 / 4), 0]])
+
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, img_size)
+    return warped, dst, src
+
+def transform2(img):
+    offset = 0 #for quick modification
+    img_size = (img.shape[1], img.shape[0])
+
+    #points in the real space
+    src = np.float32([[580,460],
+                      [700,460],
+                      [1040,680],
+                      [260,680]])
+
+    #points on the image plane
+    dst = np.float32([[260,0],
+                      [1040,0],
+                      [1040,720],
+                      [260,720]])
+
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, img_size)
     return warped, dst, src
 
 def display_image(img):
-    plt.imshow(img)
-    plt.show()
-
-def display_image_with_lines(img):
-    offset = 20
-    # img = cv2.line(img, (215-offset,700), (615-offset,435), color=(255,0,0), thickness=3)
-    # img = cv2.line(img, (1050+offset,680), (661+offset,435), color=(255,0,0), thickness=3)
-
-    img = cv2.line(img, (585-offset,460), (203-offset,720), color=(255,0,0), thickness=3)
-    img = cv2.line(img, (1127+offset,720), (695+offset,460), color=(255,0,0), thickness=3)
-    img = cv2.line(img, (585-offset,460), (695+offset,460), color=(255,0,0), thickness=3)
-    img = cv2.line(img, (203-offset,720), (1127+offset,720), color=(255,0,0), thickness=3)
     plt.imshow(img)
     plt.show()
 
